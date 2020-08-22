632b262f15233cd3d654e8b029936f1f59d2fe9c
Mat Wilmot <matwilmot90@gmail.com>
2020-08-22 14:09:11 -0700

    working form, check console for results

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
   const appID = "ab590b46";
   const appKey = "dabc414751363fff3aa8909d8747b8ba";
 
+  $("#submit").on("click", (e) => {
+    e.preventDefault();
+    let food = $("#food").val();
+    // quantity is a STRING, parseInt makes it a number
+    let quantity = parseInt($("#amount").val());
+    let unit = $("#unit").val();
+
+    console.log({
+      food: food,
+      qty: quantity,
+      unit: unit,
+    });
+
+    searchFood(food)
+      .then((food) => searchNutrients(food, quantity, unit))
+      .then((nutrients) => console.log(nutrients));
+  });
+
   // takes a string and returns the food_id
   const searchFood = (item) => {
     return new Promise((resolve, reject) => {
@@ -17,7 +35,7 @@ $(document).ready(function () {
   };
 
   // Takes the food_id returned from 'searchFood()' and uses it to get detailed nutritional info
-  const searchNutrients = (id) => {
+  const searchNutrients = (id, qty, unit) => {
     return new Promise((resolve, reject) => {
       $.ajax({
         type: "POST",
@@ -27,9 +45,8 @@ $(document).ready(function () {
         data: JSON.stringify({
           ingredients: [
             {
-              quantity: 100,
-              measureURI:
-                "http://www.edamam.com/ontologies/edamam.owl#Measure_gram",
+              quantity: qty,
+              measureURI: `http://www.edamam.com/ontologies/edamam.owl#Measure_${unit}`,
               foodId: id,
             },
           ],
@@ -43,6 +60,6 @@ $(document).ready(function () {
   };
 
   searchFood("scrambled eggs")
-    .then((food) => searchNutrients(food))
+    .then((food) => searchNutrients(food, 100, "gram"))
     .then((nutrients) => console.log(nutrients));
 });
